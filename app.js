0b3db53b0737546b9cb2313952e965b13ebadc71
BURAK ALKAN <73407527+nephtys1001@users.noreply.github.com>
2023-07-26 15:25:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+
+menu.onclick = ()=>{
+    menu.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+}
